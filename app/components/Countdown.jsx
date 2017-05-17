56d0655752b8e26ebfad33e2b4465854c798127f
Juan Di Modugno <dimodugnojuan@gmail.com>
2017-05-16 21:49:40 -0300

    Countdown test

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -5,20 +5,45 @@ var CountdownForm = require('CountdownForm');
 var Countdown = React.createClass({
   getInitialState: function () {
     return {
-      totalSeconds: 0
+      count: 0,
+      countdownStatus: 'stopped'
     };
+  },
+  startTimer: function () {
+    this.timer = setInterval(() => {
+      if (this.state.count > 0) {
+        var newCount = this.state.count - 1;
+        this.setState({
+          count: newCount
+        });
+      } else {
+        this.setState({
+          countdownStatus: 'stopped'
+        });
+      }
+    }, 1000);
+  },
+  componentDidUpdate: function (prevProps, prevState) {
+    if ( this.state.countdownStatus !== prevState.countdownStatus ) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+      }
+    }
   },
   handleSetCountdown: function(seconds) {
     this.setState({
-      totalSeconds: seconds
+      count: seconds,
+      countdownStatus: 'started'
     });
   },
   render: function () {
-    var {totalSeconds} = this.state;
+    var {count} = this.state;
     return (
       <div>
         <h4 className='text-center page-title'> Countdown </h4>
-        <Clock totalSeconds={totalSeconds}/>
+        <Clock totalSeconds={count}/>
         <CountdownForm onSetCountdown={this.handleSetCountdown}/>
       </div>
     );
